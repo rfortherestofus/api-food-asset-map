47443144f24870b0877a4268b1082f7ea7bee437
Katie Jolly <katiejolly6@gmail.com>
2021-04-14 11:40:42 -0700

    code for gathering API neighborhood stats

---
+++ data-raw/analysis-neighborhoods.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(tidycensus)
+library(leaflet)
+library(sf)
+
+# from https://data.sfgov.org/Geographic-Locations-and-Boundaries/Analysis-Neighborhoods-2010-census-tracts-assigned/bwbp-wk3r
+tracts <- read_sf("data-raw/analysis_nb_tracts_2010.geojson")
+
+# list of available tidycensus data, we want a table that has both race and ethnicity to account for hispanic/latino
+v19 <- load_variables(year = 2019, dataset = "acs5")
+
+var_labels <- v19 %>%
+  filter(str_detect(name, "B03002")) %>% # in the hisp/latino by race table
+  mutate(label = str_remove(label, "Estimate!!")) %>% # cleaner later on when the word 'Estimate' is added back
+  pull(name, name = label)
+
+race_ethnicity <- get_acs(geography = "tract", variables = var_labels, year = 2019, state = "CA", county = "San Francisco", geometry = FALSE)
+
+race_ethnicity_clean <- race_ethnicity %>%
+  pivot_wider(names_from = variable, values_from = c(estimate, moe)) %>%
+  janitor::clean_names()
+
+race_ethnicity_nb_join <- tracts %>%
+  select(nhood, geoid) %>%
+  left_join(race_ethnicity_clean, by = "geoid")
+
+api_summary_nb <- race_ethnicity_nb_join %>%
+  group_by(nhood) %>%
+  summarise(total = sum(estimate_total),
+            moe_total = moe_sum(moe_total, estimate = estimate_total),
+            asian_alone = sum(estimate_total_not_hispanic_or_latino_asian_alone),
+            moe_asian_alone = moe_sum(moe_total_hispanic_or_latino_asian_alone, estimate = estimate_total_not_hispanic_or_latino_asian_alone),
+            pac_is_alone = sum(estimate_total_hispanic_or_latino_native_hawaiian_and_other_pacific_islander_alone),
+            moe_pac_is_alone = moe_sum(moe_total_hispanic_or_latino_native_hawaiian_and_other_pacific_islander_alone, estimate = estimate_total_hispanic_or_latino_native_hawaiian_and_other_pacific_islander_alone),
+            api = sum(asian_alone, pac_is_alone),
+            moe_api = moe_sum(c(moe_asian_alone, moe_pac_is_alone), estimate = c(asian_alone, pac_is_alone))) %>%
+  ungroup() %>%
+  mutate(pct_api = api/total,
+         moe_pct_api = moe_prop(api, total, moe_api, moe_total))
