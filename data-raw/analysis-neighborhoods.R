932f3305817fddfb3f09acf066b508529dc85c9a
Katie Jolly <katiejolly6@gmail.com>
2021-04-14 12:02:05 -0700

    api estimates + basic map

--- data-raw/analysis-neighborhoods.R
+++ data-raw/analysis-neighborhoods.R
@@ -14,15 +14,23 @@ var_labels <- v19 %>%
   mutate(label = str_remove(label, "Estimate!!")) %>% # cleaner later on when the word 'Estimate' is added back
   pull(name, name = label)
 
+# pull table B03002 with the variable labels instead of numeric codes
+
 race_ethnicity <- get_acs(geography = "tract", variables = var_labels, year = 2019, state = "CA", county = "San Francisco", geometry = FALSE)
+
+# one tract per row
 
 race_ethnicity_clean <- race_ethnicity %>%
   pivot_wider(names_from = variable, values_from = c(estimate, moe)) %>%
   janitor::clean_names()
 
+# join tidycensus data to the tract-neighborhood pairings
+
 race_ethnicity_nb_join <- tracts %>%
   select(nhood, geoid) %>%
   left_join(race_ethnicity_clean, by = "geoid")
+
+# calculate summary statistics by neighborhood (plus 90% CI)
 
 api_summary_nb <- race_ethnicity_nb_join %>%
   group_by(nhood) %>%
@@ -36,4 +44,19 @@ api_summary_nb <- race_ethnicity_nb_join %>%
             moe_api = moe_sum(c(moe_asian_alone, moe_pac_is_alone), estimate = c(asian_alone, pac_is_alone))) %>%
   ungroup() %>%
   mutate(pct_api = api/total,
-         moe_pct_api = moe_prop(api, total, moe_api, moe_total))
+         moe_pct_api = moe_prop(api, total, moe_api, moe_total),
+         pct_api_clean = if_else(moe_pct_api > .3 * pct_api, NA_real_, round(pct_api * 100, 2))) # make the label NA is the moe is more than 30% of the estimate. This threshold is fairly arbitrary, but we should use *some* threshold
+
+
+# leaflet for basic analysis
+
+qpal <- colorQuantile("Blues", api_summary_nb$pct_api_clean, n = 5)
+
+leaflet(api_summary_nb) %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addPolygons(stroke = TRUE, fillOpacity = 0.8,
+              weight = 1, color = "black",
+              fillColor = ~qpal(pct_api_clean),
+              ) %>%
+  addLegend(pal = qpal, values = ~pct_api_clean, opacity = 0.7, title = "% API",
+            position = "bottomright")
