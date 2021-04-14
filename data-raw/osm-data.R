0f2d04af0e386bcf3081457f5316f981bd664d9a
Katie Jolly <katiejolly6@gmail.com>
2021-04-13 20:30:07 -0700

    add convenience stores

--- data-raw/osm-data.R
+++ data-raw/osm-data.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(osmdata) # OSM overpass API
 library(leaflet)
+library(htmltools)
 library(tigris) # census geography boundaries
 library(janitor)
 library(sf) # spatial data wrangling
@@ -9,7 +10,8 @@ library(sf) # spatial data wrangling
 
 sf_boundary <- counties(state = "California", cb = TRUE) %>%
   clean_names() %>%
-  filter(name == "San Francisco")
+  filter(name == "San Francisco") %>%
+  st_transform(4326)
 
 # Supermarket query
 
@@ -25,5 +27,23 @@ supermarkets <- supermarket_q$osm_points %>%
 
 leaflet() %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
-  addPolygons(data = sf_boundary)
-  addCircleMarkers(data = supermarkets, fillColor = "#840651")
+  addPolygons(data = sf_boundary, fillOpacity = 0, opacity = 1, color = "#FFB55F", weight = 2) %>%
+  addCircleMarkers(data = supermarkets, fillColor = "#840651", color = "#840651", opacity = 1, fillOpacity = 0.7, weight = 1, radius = 2)
+
+# Convenience store query
+
+convenience_store_q <- getbb("San Francisco") %>%
+  opq() %>%
+  add_osm_feature("shop", "convenience") %>%
+  osmdata_sf()
+
+convenience_stores <- convenience_store_q$osm_points %>%
+  st_intersection(sf_boundary) # the convenience store query includes some from outside of the city, so this limits the data to only supermarkets in SF
+
+# visually check data
+
+leaflet() %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addPolygons(data = sf_boundary, fillOpacity = 0, opacity = 1, color = "#FFB55F", weight = 2) %>%
+  addCircleMarkers(data = convenience_stores, fillColor = "#5F9AB6", color = "#5F9AB6", opacity = 1, fillOpacity = 0.7, weight = 1, radius = 2, label = ~htmlEscape(name))
+
