2b26412904b35f4fce5722ea6f5e96f42aa66186
David Keyes <david@rfortherestofus.com>
2021-05-18 10:24:53 -0700

    Add static neighborhood maps + try laying them out on home page

--- static-maps.R
+++ static-maps.R
@@ -10,13 +10,37 @@ sf_boundary <- counties(state = "California", cb = TRUE) %>%
 
 demographics <- read_rds(("data/api_neighborhood_data.rds"))
 
-ggplot() +
-  # geom_sf(data = demographics,
-  #         color = "white",
-  #         size = 0.1) +
-  geom_sf(data = filter(demographics, nhood == "Visitacion Valley"),
-          fill = "transparent",
-          color = "#AD1D32") +
-  # labs(title = "Visitacion Valley") +
-  # theme(plot.title = element_text(hjust = 0.5)) +
-  theme_void()
+neighborhoods <- demographics %>%
+  filter(nhood %in% c("SOMA", "Richmond", "Sunset", "Chinatown", "Japantown", "Bayview Hunters Point", "Tenderloin", "Visitacion Valley", "Excelsior")) %>%
+  st_drop_geometry() %>%
+  pull(nhood)
+
+title_position <- filter(demographics, nhood == "Japantown") %>%
+  st_centroid()
+
+
+
+generate_static_neighborhood_map <- function(neighborhood_name) {
+
+  file_name <- neighborhood_name %>%
+    str_replace_all(" ", "-") %>%
+    str_to_lower() %>%
+    str_glue(".svg")
+
+  ggplot(data = filter(demographics, nhood == neighborhood_name),) +
+    geom_sf(fill = "#AD1D32",
+            color = "transparent") +
+    geom_sf_text(aes(label = str_wrap(neighborhood_name, 10)),
+                 family = "Oswald",
+                 color = "white",
+                 size = 12) +
+    theme_void()
+
+  ggsave(str_glue("assets/{file_name}"))
+
+}
+
+generate_static_neighborhood_map(neighborhoods[1])
+
+walk(neighborhoods, generate_static_neighborhood_map)
+
