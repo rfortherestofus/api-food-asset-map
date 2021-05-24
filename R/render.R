d213ce70041cf6591db533984bdb79d5232a9b9f
David Keyes <david@rfortherestofus.com>
2021-05-24 13:27:17 -0700

    Render full site

--- R/render.R
+++ R/render.R
@@ -6,12 +6,12 @@ library(beepr)
 
 # Generate Assets ---------------------------------------------------------
 
-# source("R/icon-summaries.R")
-# source("R/static-maps.R")
+source("R/icon-summaries.R")
+source("R/static-maps.R")
 
 # Generate TailwindCSS ----------------------------------------------------
 
-# terminalExecute("NODE_ENV=production npx tailwindcss-cli build css/tailwind.css -o css/apistyle.css")
+terminalExecute("NODE_ENV=production npx tailwindcss-cli build css/tailwind.css -o css/apistyle.css")
 
 # Render Site -------------------------------------------------------------
 
