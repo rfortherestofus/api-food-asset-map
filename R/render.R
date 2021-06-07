84a394a7c34837bc9cc6eb6719ca2ccdfc6f61dd
David Keyes <david@rfortherestofus.com>
2021-06-07 11:18:42 -0700

    Render site with correct corner/convenience store filtering

--- R/render.R
+++ R/render.R
@@ -10,7 +10,7 @@ source("R/static-maps.R")
 
 # Generate TailwindCSS ----------------------------------------------------
 
-terminalExecute("NODE_ENV=production npx tailwindcss-cli build css/tailwind.css -o css/apistyle.css")
+# terminalExecute("NODE_ENV=production npx tailwindcss-cli build css/tailwind.css -o css/apistyle.css")
 
 # Render Site -------------------------------------------------------------
 
